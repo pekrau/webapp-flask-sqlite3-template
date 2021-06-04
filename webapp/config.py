9b5168ff5b7a6e5e9925ad3a03dc58f5bee79775
Per Kraulis <per.kraulis@gmail.com>
2021-06-04 13:57:09 +0200

    Removed DEBUG env var.

--- webapp/config.py
+++ webapp/config.py
@@ -10,12 +10,11 @@ ROOT_DIRPATH = os.path.dirname(os.path.abspath(__file__))
 
 # Default configurable values; modified by reading a JSON file in 'init'.
 DEFAULT_SETTINGS = dict(
-    SERVER_NAME = "127.0.0.1:5003",
+    SERVER_NAME = "127.0.0.1:5003",   # For URL generation; app.run() in devel.
     SITE_NAME = "webapp-sqlite3",
     SITE_STATIC_DIRPATH = None,
     SITE_ICON = None,           # Filename, must be in 'SITE_STATIC_DIRPATH'
     SITE_LOGO = None,           # Filename, must be in 'SITE_STATIC_DIRPATH'
-    DEBUG = False,
     LOG_DEBUG = False,
     LOG_NAME = "webapp",
     LOG_FILEPATH = None,
